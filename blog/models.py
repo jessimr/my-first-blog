ff1eb0cf4dbde6c1b9ce7d0ccb3e55bdc65ff147
jessimr <jessicamendizaruiz@gmail.com>
2017-05-07 11:05:31 +0200

    Mi app Django Girls, primer commit

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+#models.Model -> significa que Post es un modelo de Django y por tanto Django debe guardarlo en la base de datos
+class Post(models.Model):
+	author = models.ForeignKey('auth.User') #ForeingKey -> vinculo con otro modelo
+	title = models.CharField(max_length = 200)  #CharField -> texto con un numero limitado de caracteres (200)
+	text = models.TextField() #TextField -> para textos largos sin limites
+	created_date = models.DateTimeField(default = timezone.now) #DateTimeField -> fecha y hora
+	published_date = models.DateTimeField(blank = True, null = True)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
+
